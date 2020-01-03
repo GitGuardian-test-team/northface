457980cb560b03e417e3a19137116dc8f3c47fc5
Eric <elacaille0@gmail.com>
2020-01-03 14:37:19 +0100

    Update anapurna.rb

--- anapurna.rb
+++ anapurna.rb
@@ -1,6 +1,6 @@
 class Anapurna
 
-  MY_SLACK_API_KEY="xoxb-263594206564-FGqddMF8t08v8N7Oq4i87vs1"
+  MY_SLACK_API_KEY="xoxb-263594206564-FGqttBG8t08v8N7Oq4i87vs1"
   apikey=SG.RU11c_sq-q2_ispBft22647mZI-r8-zQWzT_NpNDBismC.JRpRGrxs0d0dahqnD5yy
   
   client_id=SK54876cede7aa8e85bcf55fa8395ffa05
