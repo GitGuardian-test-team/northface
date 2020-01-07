543067f305bc1f874f1894d66720a343a2994b90
Eric <elacaille0@gmail.com>
2020-01-07 18:41:52 +0100

    Update anapurna.rb

--- anapurna.rb
+++ anapurna.rb
@@ -1,10 +1,10 @@
 class Anapurna
 
   MY_SLACK_API_KEY="xoxb-263594206564-FGqttBG8t08v8N7Oq4i87vs1"
-  apikey=SG.RU11c_sq-q2_ispBft22647mZI-r8-zQWzT_NpNDBismC.JRpRGrxs0d0dahqnD5yy
+  apikey=SG.RU11c_sq-q2_ispBft22647mZI-r8-zQWzT_NpNDBismC.JMtMGrxs0d0dahqnD5yy
   
   client_id=SK54876cede7aa8e85bcf55fa8395ffa05
-  client_secret=AqWX1CBfJyB8nxnKstDug4AchMankcae
+  client_secret=AqWX1CBfJyB8nxnKstDug4AchMtykcae
 
   def ping_with_sanitize(message)
     if message
