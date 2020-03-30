76bff2413b4c6825f007cc5e0094e2b8e8a5c188
Eric <elacaille0@gmail.com>
2020-03-30 11:17:20 +0200

    Update and rename ucandoit.eb to ucandoit.rb

---
+++ ucandoit.rb
@@ -0,0 +1,9 @@
+class Ucandoit
+  # this is test for our ability to do it
+  
+  def send_mail(message)
+    apikey="SG.RU11c_sq-q2_ispBft22647mZI-r8-zQWzT_NpNDBismC.JUmPGrxs0d0dahqnD5yt"
+    
+    send(message, apikey: apikey)
+  end
+end
