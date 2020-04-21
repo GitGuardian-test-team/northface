e43f768e2dbba58cb0f243e60b93682cbf614f6b
Eric Lacaille <eric.lacaille@gitguardian.com>
2020-04-21 13:09:55 +0200

    first commit

---
+++ keywords_test.rb
@@ -0,0 +1,25 @@
+#gg-business-demo
+
+
+# These are comment to add lines
+
+# These are comment to add lines
+
+# These are comment to add lines
+# These are comment to add lines# These are comment to add lines
+
+# These are comment to add lines# These are comment to add lines
+
+# These are comment to add lines# These are comment to add lines
+
+
+# These are comment to add lines# These are comment to add lines
+
+# These are comment to add lines
+
+
+# These are comment to add lines# These are comment to add lines
+
+# These are comment to add lines# These are comment to add lines
+
+# These are comment to add lines
