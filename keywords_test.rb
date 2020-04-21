2d2134c90f160571f1c2fdc2adfe5ccb09af8c64
Eric Lacaille <eric.lacaille@gitguardian.com>
2020-04-21 13:16:07 +0200

    test with keysword in file content

--- keywords_test.rb
+++ keywords_test.rb
@@ -23,3 +23,9 @@
 # These are comment to add lines# These are comment to add lines
 
 # These are comment to add lines
+
+
+class KeywordTest
+
+	POST_MAN_KEY="PMAK-5e3c52895351d62230f49163-123cbbbd013341996d81dc074251e29d43"
+end
